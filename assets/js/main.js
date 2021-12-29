a99bc81f58b9578b3696f1276ca3bc3857b1dcbb
Brendon Martin <brendonmartin657@gmail.com>
2021-12-29 10:33:47 -0500

    Added in JavaScript to open and close Modal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
 /*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+  const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent);
 
+  if (openBtn && modalContainer) {
+    openBtn.addEventListener("click", () => {
+      modalContainer.classList.add("show-modal");
+    });
+  }
+};
+
+showModal("open-modal", "modal-container");
 
 /*=============== CLOSE MODAL ===============*/
+function closeModal() {
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.classList.remove("show-modal");
+}
+
+const closeBtns = document.querySelectorAll(".close-modal");
+
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", closeModal);
+});
